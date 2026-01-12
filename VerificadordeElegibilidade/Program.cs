8238c62b2c5f6991108475f81e7fa4670900f827
jrulz <jrulz@LAPTOP-K24JA05U>
2026-01-12 15:42:08 -0300

    Adicionar arquivos de projeto.

---
+++ VerificadordeElegibilidade/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {  
+        Console.WriteLine("Digite sua idade");
+        int idade = int.Parse(Console.ReadLine());
+
+
+        Console.WriteLine("Qual a sua renda mensal?");
+        double renda = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Você está empregado? (s/n)");
+        string empregada = Console.ReadLine().ToLower();
+
+        bool maiorDeIdade = idade >= 18;
+        bool rendaPermitida= renda <= 3000;
+        bool estaEmpregada = empregada == "s" || empregada == "sim";
+
+        if (maiorDeIdade && rendaPermitida && estaEmpregada)
+        {
+            Console.WriteLine("✅ Você está elegível.");
+        }
+        else
+        {
+            Console.WriteLine("❌ Você não está elegível.");
+        }
+    }
+}
+
